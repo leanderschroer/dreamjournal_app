aeb8cc49aa4dd2d198fed3fdaa19ef04a5368f84
Leander Schroer <leander.schroer@gmail.com>
2020-01-19 17:27:28 +0100

    kivy setup

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+kivy.require('1.11.1')
+
+
+class Manager(ScreenManager):
+    pass
+
+
+class HomeScreen(BoxLayout):
+    pass
+
+
+class LoginScreen(GridLayout):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return Manager()
+
+
+if __name__ == '__main__':
+    MainApp().run()
